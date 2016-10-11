aabf70518915cd3e6496d07ffee58dc2bcce77e8
Benjamin Debotté <benjamin.debotte@gmail.com>
2016-10-11 18:27:15 +0200

    Fixed the front : bad typo over 'enctype' which should have benn 'encType'

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,17 +9,10 @@ class App extends Component {
   render() {
     return (
         <div className="App">
-            <div>
-                <img src='primitive.jpg' className="App-logo" alt="logo" />
-            </div>
-            <div className="col-md-6 col-md-offset-6">
-            <form enctype="multipart/form-data" action="http://localhost:8080/api/photos/upload" method="POST">
-                Choose a file to upload: 
-                <input name="photo" type="file" /><br />
-                <input type="submit" value="Upload File" />
-                </form>
-            </div>
-
+            <form action="http://localhost:8080/api/photos/upload" method="post" encType="multipart/form-data">
+              <input type="file" name ="photo" accept="application/x-zip-compressed,image/*"/>
+              <input type="submit" value="Upload selected file to server"/>
+            </form>
         </div>
     );
   }
