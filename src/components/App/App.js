b3216cd71daca761cfc00e90614dc6189ff512f3
Benjamin Debotté <benjamin.debotte@gmail.com>
2016-10-23 23:01:58 +0200

    Added bootstrap and style over a form Dynamic reaching of the converted sent image, displaying it under the form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,13 +8,23 @@ import Button from 'react-button';
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            convertedImgLink: '',
+            baseImg: '',
+            buttonText: 'Browse..'
+        }
+    }
     onClick(file) {
+
         var files = document.getElementById('file_to_upload').files;
         var formData = new FormData();
 
         if (files.hasOwnProperty(0) && files[0] instanceof File)
             formData.append('photo', files[0]);
 
+            this.setState({buttonText: 'Sending..'});
 
 
         var req = request
@@ -29,21 +39,49 @@ class App extends Component {
                   console.log(err);
               }
               console.log(res);
+              var body = JSON.parse(res.text);
+              console.log(body);
+              this.setState({convertedImgLink: body.url});
+              console.log(this.state);
 
           });
 }
 
   render() {
+     const thumbnail = (this.state.convertedImgLink !== '' ? (
+     <div className="row">
+     <div className="col-md-4">
+     </div>
+       <div className="col-md-4">
+         <a href="#" className="thumbnail">
+           <img src={this.state.convertedImgLink} width="200px" alt=""/>
+         </a>
+       </div>
+     </div>
+ ) : "" );
     return (
-      <div className="App">
-        <div className="App-header">
+
+      <div className="container App">
+        <div className="row App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>PicShape</h2>
         </div>
-        <form>
-            <input type='file' encType='multipart/form-data' id='file_to_upload' name='photo' />
-        </form>
-        <Button onClick={this.onClick.bind(this)} >Send</Button>
+        <div className="row">
+            <div className="col-md-4">
+            </div>
+
+            <div className="col-md-4 col-md-offset-4">
+                <form>
+                    <div class="form-horizontal">
+                        <label className="btn btn-primary btn-file"> {this.state.buttonText}
+                        <input type='file' encType='multipart/form-data' id='file_to_upload' name='photo' style={{display: "none"}}/>
+                        </label>
+                    </div>
+                    </form>
+                    <Button onClick={this.onClick.bind(this)} >Send</Button>
+            </div>
+        </div>
+        {thumbnail}
 
       </div>
     );
