90e2c9960b441850a1f98607f982695e9dc91a3e
Benjamin Debotté <benjamin.debotte@gmail.com>
2016-11-09 13:31:51 +0100

    Profile design updated

--- src/components/Account/Profile.js
+++ src/components/Account/Profile.js
@@ -41,55 +41,54 @@ class Profile extends React.Component {
 
     return (
       <div className="container">
+
         <div className="panel">
           <div className="panel-body">
             <form onSubmit={this.handleProfileUpdate.bind(this)} className="form-horizontal">
               <legend>Profile Information</legend>
-              <div className="form-group">
-                <label htmlFor="email" className="col-sm-3">Email</label>
-                <div className="col-sm-7">
-                  <input type="email" name="email" id="email" className="form-control" value={this.state.email} onChange={this.handleChange.bind(this)}/>
-                </div>
+              <div className="form-group row">
+                <label htmlFor="email">Email</label>
+                <div class="col-xs-10">
+                 <input type="email" name="email" id="email" className="form-control" value={this.state.email} onChange={this.handleChange.bind(this)}/>
+                 </div>
               </div>
-              <div className="form-group">
-                <label htmlFor="name" className="col-sm-3">Name</label>
-                <div className="col-sm-7">
+              <div className="form-group row">
+                <label htmlFor="name" >Name</label>
+                <div class="col-xs-10">
                   <input type="text" name="name" id="name" className="form-control" value={this.state.name} onChange={this.handleChange.bind(this)}/>
                 </div>
               </div>
-              <div className="form-group">
-                <label className="col-sm-3">Gender</label>
-                <div className="col-sm-4">
-                  <label className="radio-inline radio col-sm-4">
-                    <input type="radio" name="gender" value="male" checked={this.state.gender === 'male'} onChange={this.handleChange.bind(this)}/><span>Male</span>
-                  </label>
-                  <label className="radio-inline col-sm-4">
-                    <input type="radio" name="gender" value="female" checked={this.state.gender === 'female'} onChange={this.handleChange.bind(this)}/><span>Female</span>
-                  </label>
-                </div>
+              <div className="form-group row">
+                <label>Gender</label>
+                <div class="col-xs-10">
+                <label className="radio-inline radio col-sm-4">
+                  <input type="radio" className="form-check-input" name="gender" value="male" checked={this.state.gender === 'male'} onChange={this.handleChange.bind(this)}/><span>Male</span>
+                </label>
+                <label className="radio-inline col-sm-4">
+                  <input type="radio" className="form-check-input" name="gender" value="female" checked={this.state.gender === 'female'} onChange={this.handleChange.bind(this)}/><span>Female</span>
+                </label>
+                 </div>
               </div>
-              <div className="form-group">
-                <label htmlFor="location" className="col-sm-3">Location</label>
-                <div className="col-sm-7">
+              <div className="form-group row">
+                <label htmlFor="location" >Location</label>
+                <div class="col-xs-10">
                   <input type="text" name="location" id="location" className="form-control" value={this.state.location} onChange={this.handleChange.bind(this)}/>
-                </div>
+                  </div>
               </div>
-              <div className="form-group">
-                <label htmlFor="website" className="col-sm-3">Website</label>
-                <div className="col-sm-7">
-                  <input type="text" name="website" id="website" className="form-control" value={this.state.website} onChange={this.handleChange.bind(this)}/>
-                </div>
+              <div className="form-group row">
+                <label htmlFor="website" >Website</label>
+                <div class="col-xs-10">
+              <input type="text" name="website" id="website" className="form-control" value={this.state.website} onChange={this.handleChange.bind(this)}/>
               </div>
-              <div className="form-group">
+              </div>
+              <div className="form-group row">
                 <label className="col-sm-3">Gravatar</label>
                 <div className="col-sm-4">
                   <img src={this.state.gravatar} width="100" height="100" className="profile"/>
                 </div>
               </div>
               <div className="form-group">
-                <div className="col-sm-offset-3 col-sm-4">
                   <button type="submit" className="btn btn-success">Update Profile</button>
-                </div>
               </div>
             </form>
           </div>
@@ -99,26 +98,26 @@ class Profile extends React.Component {
             <form onSubmit={this.handleChangePassword.bind(this)} className="form-horizontal">
               <legend>Change Password</legend>
               <div className="form-group">
-                <label htmlFor="password" className="col-sm-3">New Password</label>
+                <label htmlFor="password" >New Password</label>
                 <div className="col-sm-7">
                   <input type="password" name="password" id="password" className="form-control" value={this.state.password} onChange={this.handleChange.bind(this)}/>
                 </div>
               </div>
               <div className="form-group">
-                <label htmlFor="confirm" className="col-sm-3">Confirm Password</label>
+                <label htmlFor="confirm" >Confirm Password</label>
                 <div className="col-sm-7">
                   <input type="password" name="confirm" id="confirm" className="form-control" value={this.state.confirm} onChange={this.handleChange.bind(this)}/>
                 </div>
               </div>
               <div className="form-group">
-                <div className="col-sm-4 col-sm-offset-3">
+                <div className=" col-sm-offset-3">
                   <button type="submit" className="btn btn-success">Change Password</button>
                 </div>
               </div>
             </form>
           </div>
         </div>
-        
+
         <div className="panel">
           <div className="panel-body">
             <form onSubmit={this.handleDeleteAccount.bind(this)} className="form-horizontal">
