e39a87fe254c2147a27bbc323ec8e3d2cdc46533
Benjamin Debotté <benjamin.debotte@gmail.com>
2016-11-09 11:37:17 +0100

    Reworked Upload component

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -85,53 +85,69 @@ class Upload extends Component {
 
   render() {
      const thumbnail = (this.state.convertedImgLink !== '' ? (
-     <div className="row">
-       <div className="colonne_2">
-         <a className="thumbnail">
-          <img onClick={this._changeImage.bind(this)} id="convertedImg" src={this.state.convertedImgLink} width="700px" alt="" onMouseOver=""/>
-         </a>
-       </div>
-     </div>
- ) : <div className="colonne_2 upload" >
-  <div className="fill"></div>
-  <a>
-  Choose a picture !
-  </a>
-  <div className="fill"></div>
- </div> );
+                 <a className="thumbnail">
+                  <img className="img-responsive center-block" onClick={this._changeImage.bind(this)} id="convertedImg" src={this.state.convertedImgLink} width="70%" alt="" onMouseOver=""/>
+                 </a>
+         ) : (
+             <div className="colonne_2 upload" >
+                  <div className="fill"></div>
+                      <p>
+                      Choose a picture !
+                      </p>
+                  <div className="fill"></div>
+             </div>
+            )
+     );
 
     return (
-      <div className="container upload">
-        {thumbnail}
-        <div className="row">
-            <div className="colonne col-md-offset-4">
-                <form>
-                    <div className="form-horizontal">
-                        <label className="btn btn-primary btn-file"> {this.state.buttonText}
-                        <input onChange={this._handleImageChange.bind(this)} type='file' encType='multipart/form-data' id='file_to_upload' name='photo' style={{display: "none"}}/>
-                        </label>
-                        <br></br>
-                        Iteration :
-                        <input id="iter" type="number" min="1" max="500" /><br></br>
-                        Mode :
-                        <select id="mode">
-                          <option value="0">Combo</option>
-                          <option value="1">Triangle</option>
-                          <option value="2">Rectangle</option>
-                          <option value="3">Ellipse</option>
-                          <option value="4">Circle</option>
-                          <option value="5">Rotatedrect</option>
-                          <option value="6">Beziers</option>
-                          <option value="7">Rotatedellipse</option>
-                          <option value="8">Polygon</option>
-                        </select><br></br><br></br>
-
-                    </div>
-                    </form>
-                    <Button onClick={this.onClick.bind(this)} >Send</Button>
+      <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-4"></div>
+                <div className="col-md-4">
+                    {thumbnail}
+                </div>
             </div>
-        </div>
+            <div className="row">
+                <div className="col-md-4"></div>
+                <div className="col-md-4 center-block">
+                    <form>
+                        <div className="form-group">
+                            Iteration :
+                            <input className="form-control" id="iter" type="number" min="1" max="500"/>
+                            Mode :
+                            <select className="form-control" id="mode">
+                              <option value="0">Combo</option>
+                              <option value="1">Triangle</option>
+                              <option value="2">Rectangle</option>
+                              <option value="3">Ellipse</option>
+                              <option value="4">Circle</option>
+                              <option value="5">Rotatedrect</option>
+                              <option value="6">Beziers</option>
+                              <option value="7">Rotatedellipse</option>
+                              <option value="8">Polygon</option>
+                            </select>
 
+                        </div>
+
+
+                        <div className="row">
+                            <div className="col-md-6">
+                                <label className="btn btn-primary btn-file"> {this.state.buttonText}
+                                    <input className="form-control" onChange={this._handleImageChange.bind(this)} type='file' encType='multipart/form-data' id='file_to_upload' name='photo' style={{display: "none"}}/>
+                                </label>
+                            </div>
+                            <div className="col-md-6">
+                                <Button onClick={this.onClick.bind(this)} >Send</Button>
+                            </div>
+                        </div>
+
+                        </form>
+
+
+
+
+                </div>
+            </div>
       </div>
     );
   }
